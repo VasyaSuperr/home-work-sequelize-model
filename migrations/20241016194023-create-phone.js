a710867233e92863719633a70092826a0890555c
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-10-18 14:28:22 +0300

    Add migrations

--- migrations/20241016194023-create-phone.js
+++ migrations/20241016194023-create-phone.js
@@ -1,46 +1,91 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Phones', {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('phones', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       model: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(100),
+        allowNull: false,
       },
       brand: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(50),
+        allowNull: false,
       },
-      yearOfManufacture: {
-        type: Sequelize.DATEONLY
+      year_of_manufacture: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
       },
-      ramSize: {
-        type: Sequelize.INTEGER
+      ram_size: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       processor: {
-        type: Sequelize.STRING
-      },
-      screenSize: {
-        type: Sequelize.FLOAT
-      },
-      hasNFC: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
+        type: Sequelize.STRING(100),
         allowNull: false,
-        type: Sequelize.DATE
       },
-      updatedAt: {
+      screen_size: {
+        type: Sequelize.FLOAT,
         allowNull: false,
-        type: Sequelize.DATE
-      }
+      },
+      has_nfc: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.addConstraint('phones', {
+      fields: ['model'],
+      type: 'check',
+      where: Sequelize.literal(
+        'CHAR_LENGTH(model) >= 2 AND CHAR_LENGTH(model) <= 100'
+      ),
+    });
+
+    await queryInterface.addConstraint('phones', {
+      fields: ['brand'],
+      type: 'check',
+      where: Sequelize.literal(
+        'CHAR_LENGTH(brand) >= 2 AND CHAR_LENGTH(brand) <= 50'
+      ),
+    });
+
+    await queryInterface.addConstraint('phones', {
+      fields: ['year_of_manufacture'],
+      type: 'check',
+      where: {
+        year_of_manufacture: {
+          [Sequelize.Op.lte]: Sequelize.literal('CURRENT_DATE'),
+        },
+      },
+    });
+
+    await queryInterface.addConstraint('phones', {
+      fields: ['ram_size'],
+      type: 'check',
+      where: { ram_size: { [Sequelize.Op.between]: [2, 128] } },
+    });
+
+    await queryInterface.addConstraint('phones', {
+      fields: ['screen_size'],
+      type: 'check',
+      where: { screen_size: { [Sequelize.Op.between]: [2.0, 10.0] } },
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Phones');
-  }
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('phones');
+  },
 };
