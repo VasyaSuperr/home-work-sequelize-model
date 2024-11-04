176e31c42558cd9c971a293cbf9eb16e7900f5ee
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-11-04 17:54:05 +0200

    Add filtration on getPreorders

--- controllers/preordersController.js
+++ controllers/preordersController.js
@@ -1,14 +1,22 @@
 const { Preorder, Phone } = require('./../models');
 module.exports.getPreorders = async (req, res, next) => {
+  const { status } = req.query;
+
   try {
-    const foundPreorders = await Preorder.findAll({
+    const filterOption = {
       attributes: { exclude: ['createdAt', 'updatedAt'] },
       include: {
         model: Phone,
         attributes: ['model', 'brand', 'processor'],
       },
       raw: true,
-    });
+    };
+
+    if (status) {
+      filterOption.where = { status };
+    }
+
+    const foundPreorders = await Preorder.findAll(filterOption);
 
     res.status(200).send({ data: foundPreorders });
   } catch (error) {
